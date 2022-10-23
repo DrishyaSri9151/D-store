e71e2a4ddab20d3acad12edd5ebd88faa3d2944f
drishyasrivastava <srivastavadrishya1@gmail.com>
2022-10-24 02:15:48 +0530

    Day 4 Completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,21 +1,60 @@
 import 'package:flutter/material.dart';
 
-
 class Loginpage extends StatelessWidget {
-  const Loginpage({ Key key }) : super(key: key);
+  const Loginpage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Log IN Page",
-        style: TextStyle(
-           fontSize: 20,
-           color: Colors.blue,
-           fontWeight: FontWeight.bold,
-        ),
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "welcome",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter UserName",
+                      labelText: "UserName",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("hi Drishya");
+                    },
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
