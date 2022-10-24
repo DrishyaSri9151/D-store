f5b7387d7e402444021a0226b140fa8df94301fe
drishyasrivastava <srivastavadrishya1@gmail.com>
2022-10-24 16:45:28 +0530

    Day 6 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,19 +2,18 @@ import 'package:flutter/material.dart';
 
 
 class Homepage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-        body: Center(
-          child: Container(
-            child: Text("hello Drishya"),
-          ),
+      body: Center(
+        child: Container(
+          child: Text("hello Drishya}"),
         ),
-        drawer: Drawer(),
-      );
+      ),
+      drawer: Drawer(),
+    );
   }
 }
